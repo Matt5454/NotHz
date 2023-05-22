5c13351d0590b90d532ea015939c388354591e7a
Matt5454 <tmatousek14@gmail.com>
2023-05-22 10:26:07 +0200

    initial

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Phoenix RefreshRate Control
+# Something RefreshRate Control
 
-Simple app to change Poco X2 Refresh rate (60Hz/90Hz/120Hz) without ADB. Thanks to Max Weinbach for finding this tweak and Rohit Shetty for testing this app.
+Simple app to change Nothing Phone (1) Refresh rate (60Hz/90Hz/120Hz) without ADB. 
+Thanks to Max Weinbach for finding this tweak, Rohit Shetty for testing this app and everyone who worked on PhoenixRefreshRateControl which this is based on.
 
 This app also includes 3 qs tiles for setting 60Hz, 90Hz and 120Hz. Check in your quick settings panel to add them.
 
 Info: This app targets older android sdk in order to set refresh rates without ADB requirement. Ignore the dialog that says "This app was built for an older version of Android...."
-
-### Screenshots
-
-![Screenshot 1](https://github.com/devabhixda/PhoenixRefreshRateControl/blob/master/PhoenixRefreshRateControl.png)
-
-
 
--- app/src/main/java/devabhixda/phoenix/refresh/ControllerTile120.kt
+++ app/src/main/java/devabhixda/phoenix/refresh/ControllerTile120.kt
@@ -5,20 +5,55 @@ import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
+
 @SuppressLint("NewApi")
 class ControllerTile120 : TileService() {
+    private var currentRefreshRate = RefreshRate.HundredTwenty
+
     override fun onStartListening() {
         super.onStartListening()
         val tile = qsTile
-        tile.label = getString(R.string.set_120hz)
+        tile.label = getRefreshRateLabel(currentRefreshRate)
         tile.state = Tile.STATE_ACTIVE
-        tile.icon = Icon.createWithResource(this, R.drawable.ic_refresh_black_24dp)
+        tile.icon = getRefreshRateIcon(currentRefreshRate)
         tile.updateTile()
     }
 
     override fun onClick() {
         super.onClick()
-        setRefreshRate(RefreshRate.HundredTwenty)
+        currentRefreshRate = getNextRefreshRate(currentRefreshRate)
+        setRefreshRate(currentRefreshRate)
+        updateTile()
     }
 
+    private fun getNextRefreshRate(refreshRate: RefreshRate): RefreshRate {
+        return when (refreshRate) {
+            RefreshRate.Sixty -> RefreshRate.Ninety
+            RefreshRate.Ninety -> RefreshRate.HundredTwenty
+            RefreshRate.HundredTwenty -> RefreshRate.Sixty
+        }
+    }
+
+    private fun getRefreshRateLabel(refreshRate: RefreshRate): String {
+        return when (refreshRate) {
+            RefreshRate.Sixty -> getString(R.string.set_60hz)
+            RefreshRate.Ninety -> getString(R.string.set_90hz)
+            RefreshRate.HundredTwenty -> getString(R.string.set_120hz)
+        }
+    }
+
+    private fun getRefreshRateIcon(refreshRate: RefreshRate): Icon {
+        return when (refreshRate) {
+            RefreshRate.Sixty -> Icon.createWithResource(this, R.drawable.ic_refresh_black_24dp)
+            RefreshRate.Ninety -> Icon.createWithResource(this, R.drawable.ic_refresh_black_24dp)
+            RefreshRate.HundredTwenty -> Icon.createWithResource(this, R.drawable.ic_refresh_black_24dp)
+        }
+    }
+
+    private fun updateTile() {
+        val tile = qsTile
+        tile.label = getRefreshRateLabel(currentRefreshRate)
+        tile.icon = getRefreshRateIcon(currentRefreshRate)
+        tile.updateTile()
+    }
 }
